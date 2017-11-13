8251d97748217efb4e7531e72f7b145111a2beb0
Martin Ševčík <sevca99@gmail.com>
2017-11-13 18:19:30 +0100

    13.11

---
+++ Evidence_osob_server/JsonParsse/JsonParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Evidence_osob_server.Interface;
+using Newtonsoft.Json;
+
+namespace Evidence_osob_server.JsonParsse
+{
+    class JsonParser : IParse
+    {
+        public async Task<T> ParseString<T>(string response)
+        {
+            return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<T>(response));
+        }
+    }
+}
