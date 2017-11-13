01ed9eea17c363b544bb55db2bf118e9d2d1769c
Martin Ševčík <sevca99@gmail.com>
2017-11-13 18:38:46 +0100

    13.11

---
+++ Evidence_osob_server/NewUser.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Evidence_osob_server.Entity;
+using Evidence_osob_server.Framy;
+using Evidence_osob_server.Interface;
+using Evidence_osob_server.JsonParsse;
+
+namespace Evidence_osob_server
+{
+    /// <summary>
+    /// Interakční logika pro NewUser.xaml
+    /// </summary>
+    public partial class NewUser : Page
+    {
+        public NewUser()
+        {
+            InitializeComponent();
+        }
+
+        private void created_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void back_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
