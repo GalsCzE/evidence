55eaaf586d52706460c135753f6194f53fbbc2db
Martin Ševčík <sevca99@gmail.com>
2017-11-13 18:23:30 +0100

    13.11

---
+++ Evidence_osob_server/Framy/Front.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Evidence_osob_server.Framy
+{
+    public class Front
+    {
+        public Frame frame { get; set; }
+    }
+}
